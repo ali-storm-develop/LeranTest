d1aa4acf3481e7f50e72350ecc6dc87a3bc29bda
AlienStorm4 <ali.sta.storm@gmail.com>
2019-07-18 09:25:53 +0430

    Initial commit

---
+++ app/src/main/java/stormstudio/lerantest/MainActivity.java
@@ -0,0 +1,26 @@
+package stormstudio.lerantest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv  = findViewById(R.id.imageView_W);
+
+
+        iv.setImageResource(R.drawable.untitled);
+
+        }
+
+
+}
---
+++ app/src/main/java/stormstudio/lerantest/actv_2.java
@@ -0,0 +1,18 @@
+package stormstudio.lerantest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class actv_2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle save)
+    {
+        super.onCreate(save);
+        setContentView(R.layout.actv_2);
+
+    }
+
+
+    }
